96753f9e5009c4ddba8ca2870cfd42e69a484d20
Gamy <flokygamy@gmail.com>
2021-07-07 14:23:00 +0000

    first javascript practice

---
+++ myscript.js
@@ -0,0 +1,26 @@
+//modulu arthemetic
+let number1 = 0.9;
+let number2 = 5;
+let moduluresult="";
+
+moduluresult = number1 % number2;
+ console.log(moduluresult);
+// pres and post increment
+ number2++;
+console.log(number2);
+// pres and post decrement
+number2--;
+console.log(number2);
+//pres and post incremention before usage
+
+++number2;
+console.log(number2);
+//pres and post decremention before usage
+-- number2;
+console.log(number2);
+// comparison && boolean 
+console.log(!(8 < 9) && ('a===b' || !(7> 10)));
+// boolean and string
+console.log('' == false);
+console.log(true == 'true');
+console.log(8 === 9);
